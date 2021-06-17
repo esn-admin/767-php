d12267fba5a9feab2b4ec703a9b4ca102710b185
Sam <admin@esn.paris>
2021-06-17 17:30:02 +0200

    ajout de la nav et de plusieurs pages

---
+++ 19-projet/inc/header.inc.php
@@ -0,0 +1,29 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?= URL ;?>">E-Commerce</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="<?= URL; ?>inscription.php">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URL; ?>connexion.php">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URL; ?>profil.php">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= URL; ?>produit.php">Produit</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+
---
+++ 19-projet/inscription.php
@@ -0,0 +1,20 @@
+<?php
+
+    include("inc/init.inc.php");
+    include("inc/functions.inc.php");
+
+    include("inc/head.inc.php");
+    include("inc/header.inc.php");
+
+    ?>
+    
+    <main>Je suis la page d'inscription</main>
+
+    <?php
+
+
+    include("inc/footer.inc.php");
+
+    
+
+   
